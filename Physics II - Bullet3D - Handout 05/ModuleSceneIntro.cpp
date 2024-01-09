7da12ba83d540859a8e2983733d1bbbbd2c1385b
SalvaBernades <125651873+SalvaBernades@users.noreply.github.com>
2024-01-09 10:04:49 +0100

    Primer tramo de la pista

--- Physics II - Bullet3D - Handout 05/ModuleSceneIntro.cpp
+++ Physics II - Bullet3D - Handout 05/ModuleSceneIntro.cpp
@@ -22,13 +22,14 @@ bool ModuleSceneIntro::Start()
 	App->camera->LookAt(vec3(0, 0, 0));
 
 	//App->physics->Ground(500, 500, 0, 0, 0);
-	// Aqui
-	//Para la posicion, el eje 0,0,0 del cuadrado esta en el centro de este o en una esquina
-	// el 0 0 0 es el centro del coche pero hemos subido las rampas pa q esten a 6 de distancia del suelo
-	// con esto puedes ir haciendo perfe
 	// Mira te explico	  (length, width, {posicion}, {color}, TIPO DE CUADRADO)
 	App->physics->RectRoad(80, 20, { 0, 6, 0 }, { 0,0,0,0 }, FORWARD_RECT);
-	App->physics->RectRoad(80, 20, { 0, 46, 10 }, { 0,0,0,0 }, FORWARD_RECT);
+	App->physics->RectRoad(80, 20, { 0, 6, 80 }, { 0,0,0,0 }, FORWARD_RECT);
+	App->physics->RectRoad(80, 20, { 0, 18, 155 }, { 0,0,0,0 }, INVERSE_RAMP);
+	App->physics->RectRoad(80, 20, { 0, 18, 255 }, { 0,0,0,0 }, RAMP);
+	App->physics->RectRoad(80, 20, { 0, 8, 305 }, { 0,0,0,0 }, FORWARD_RECT);
+	//Necesito poder modificar el angulo del suelp ara poder hacer giros
+	
 
 	return ret;
 }
