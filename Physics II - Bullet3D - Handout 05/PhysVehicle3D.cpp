b8695ea3c8a0757cad93f89a71566fdd8fa565f8
joanmarquesbesses <85459149+joanmarquesbesses@users.noreply.github.com>
2024-01-13 18:16:15 +0100

    coche

--- Physics II - Bullet3D - Handout 05/PhysVehicle3D.cpp
+++ Physics II - Bullet3D - Handout 05/PhysVehicle3D.cpp
@@ -44,12 +44,47 @@ void PhysVehicle3D::Render()
 	btVector3 offset(info.chassis_offset.x, info.chassis_offset.y, info.chassis_offset.z);
 	offset = offset.rotate(q.getAxis(), q.getAngle());
 
+	Cube FrontChassis(info.front_chassis_size.x, info.front_chassis_size.y, info.front_chassis_size.z);
+	vehicle->getChassisWorldTransform().getOpenGLMatrix(&FrontChassis.transform);
+	q = vehicle->getChassisWorldTransform().getRotation();
+	btVector3 offsetFront(info.front_chassis_offset.x, info.front_chassis_offset.y, info.front_chassis_offset.z);
+	offsetFront = offsetFront.rotate(q.getAxis(), q.getAngle());
+
+	Cube RearChassis(info.rear_chassis_size.x, info.rear_chassis_size.y, info.rear_chassis_size.z);
+	RearChassis.color = Red;
+	vehicle->getChassisWorldTransform().getOpenGLMatrix(&RearChassis.transform);
+	q = vehicle->getChassisWorldTransform().getRotation();
+	btVector3 offsetRear(info.rear_chassis_offset.x, info.rear_chassis_offset.y, info.rear_chassis_offset.z);
+	offsetRear = offsetRear.rotate(q.getAxis(), q.getAngle());
+
+	Sphere cabine(info.cabine_radius);
+	cabine.color = Red;
+	vehicle->getChassisWorldTransform().getOpenGLMatrix(&cabine.transform);
+	q = vehicle->getChassisWorldTransform().getRotation();
+	btVector3 offsetCabine(info.cabine_offset.x, info.cabine_offset.y, info.cabine_offset.z);
+	offsetCabine = offsetCabine.rotate(q.getAxis(), q.getAngle());
+
 	chassis.transform.M[12] += offset.getX();
 	chassis.transform.M[13] += offset.getY();
 	chassis.transform.M[14] += offset.getZ();
 
+	FrontChassis.transform.M[12] += offsetFront.getX();
+	FrontChassis.transform.M[13] += offsetFront.getY();
+	FrontChassis.transform.M[14] += offsetFront.getZ();
+
+	RearChassis.transform.M[12] += offsetRear.getX();
+	RearChassis.transform.M[13] += offsetRear.getY();
+	RearChassis.transform.M[14] += offsetRear.getZ();
+
+	cabine.transform.M[12] += offsetCabine.getX();
+	cabine.transform.M[13] += offsetCabine.getY();
+	cabine.transform.M[14] += offsetCabine.getZ();
+
 
 	chassis.Render();
+	FrontChassis.Render();
+	RearChassis.Render();
+	cabine.Render();
 }
 
 // ----------------------------------------------------------------------------
